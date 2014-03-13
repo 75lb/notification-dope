3d7085ed34bba98f2fa16ae58f4e89cd67431abc
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-03-13 16:33:38 +0100

    web notifications

--- notification-dope.js
+++ notification-dope.js
@@ -1,18 +1,42 @@
 var spawn = require("child_process").spawn,
     EventEmitter = require("events").EventEmitter,
-    util = require("util");
+    WebNotification,
+    util = require("util"),
+    permission;
+
+if (typeof window !== "undefined"){
+    WebNotification = window.Notification;
+}
 
 module.exports = Notification;
 
 function Notification(options){
+    options.title = options.title || "";
+    options.message = options.message || "";
+    options.sound = options.sound || "";
+    
     var self = this;
-    var handle = spawn("terminal-notifier", [ 
-        "-title", options.title, 
-        "-message", options.message,
-        "-sound", options.sound
-    ]);
-    handle.on("error", function(err){
-        self.emit("error", err);
-    });
+    if (WebNotification){
+        if (permission === "granted"){
+            new WebNotification(options.title, { body: options.message });
+        }
+    } else {
+        var handle = spawn("terminal-notifier", [ 
+            "-title", options.title, 
+            "-message", options.message,
+            "-sound", options.sound
+        ]);
+        handle.on("error", function(err){
+            self.emit("error", err);
+        });
+    }
 }
 util.inherits(Notification, EventEmitter);
+
+Notification.requestPermission = function(){
+    if (WebNotification){
+        WebNotification.requestPermission(function(perms){
+            permission = perms;
+        });    
+    }
+};
